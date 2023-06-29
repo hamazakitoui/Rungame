200e45614fd6e1ddaccc70ff70ff4e5480d77401
nkc20206024 <ek2070@mailg.denpa.ac.jp>
2023-06-29 15:19:18 +0900

    スコア保存処理再修正

--- RunGame/Assets/Scripts/System/GoalPoint.cs
+++ RunGame/Assets/Scripts/System/GoalPoint.cs
@@ -12,14 +12,12 @@ public class GoalPoint : MonoBehaviour
     [Header("結果表示時間")] [SerializeField] private float clearWait = 3.0f;
     [Header("セレクトシーン")] [SerializeField] private SceneObject selectScene;
     /// <summary> クリアコルーチン </summary>
-    /// <param name="score">スコア</param>
     /// <returns></returns>
-    private IEnumerator ClearFade(int score)
+    private IEnumerator ClearFade()
     {
         yield return new WaitForSeconds(clearWait); // 一定時間待機
         SaveData data = SaveManager.Instance.GetData; // セーブデータ
         data.SetStageClear(stageNum, true); // クリアに変更
-        if (data.GetStageScore(stageNum) < score) data.SetScore(stageNum, score); // スコア保存
         SaveManager.Instance.Save(data); // セーブ
         FadeSceneManager.Instance.LoadScene(selectScene); // セレクトシーンに移動
     }
@@ -31,16 +29,14 @@ public class GoalPoint : MonoBehaviour
         yield return new WaitForSeconds(clearWait); // 一定時間待機
         SaveData data = SaveManager.Instance.GetData; // セーブデータ
         data.SetStageClear(stageNum, true); // クリアに変更
-        if (data.GetStageScore(stageNum) < score) data.SetScore(stageNum, score); // スコア保存
         SaveManager.Instance.Save(data); // セーブ
         FadeSceneManager.Instance.LoadScene(selectScene); // セレクトシーンに移動
     }
     /// <summary> ゴール </summary>
-    /// <param name="score">スコア</param>
-    public void GameClear(int score)
+    public void GameClear()
     {
         if (isClear) return; // 一回だけ実行
-        StartCoroutine(ClearFade(score)); // クリア
+        StartCoroutine(ClearFade()); // クリア
         isClear = true;
     }
     /// <summary> ゴール </summary>
@@ -50,5 +46,17 @@ public class GoalPoint : MonoBehaviour
         if (isClear) return; // 一回だけ実行
         StartCoroutine(ClearFade(stageNum, score)); // クリア
         isClear = true;
+    }
+    /// <summary> 最高スコア保存 </summary>
+    /// <param name="score">スコア</param>
+    public void SaveScore(int score)
+    {
+        SaveData data = SaveManager.Instance.GetData; // セーブデータ
+        // ハイスコアを更新したら
+        if (data.GetStageScore(stageNum) < score)
+        {
+            data.SetScore(stageNum, score);
+            SaveManager.Instance.Save(data); // セーブ
+        }
     }
 }
