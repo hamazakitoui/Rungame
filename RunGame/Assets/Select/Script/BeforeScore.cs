aa65e1c9e74e461bb7f93f2af05c2e7c6461890d
hamazakitoui <nkc20206045@denpa.jp>
2023-07-05 12:11:16 +0900

    セレクトシーンのスコア表示

---
+++ RunGame/Assets/Select/Script/BeforeScore.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BeforeScore : MonoBehaviour
+{
+    SaveData Data;
+    [SerializeField] Sprite NotAchievement;
+    [SerializeField] Sprite GetAchievement;
+    [SerializeField] GameObject[] Achevementobject;
+    [SerializeField] text[] textobject;
+    int nowCliar;
+    const int MaxStage = 5;
+    const int MaxAchievement = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SaveManager.Instance.Load();
+        Data = SaveManager.Instance.GetData;
+
+        for (int Stage = 0; MaxStage > Stage; Stage++)
+        {
+            Achevementobject[Stage].SetActive(true);
+            if (Data.GetStageClear(Stage))
+            {
+                
+                nowCliar++;
+                
+
+                for (int Ahevement = 0; Ahevement <= MaxAchievement; Ahevement++)
+                {
+                    bool checkAchevement = Data.GetAchievement(Stage, Ahevement);
+                    Debug.Log(Data.GetAchievement(Stage,Ahevement));
+                    if (checkAchevement)
+                    {
+                        Debug.Log("abc");
+                        Achevementobject[Stage].transform.GetChild(Ahevement).GetComponent<Image>().sprite = GetAchievement;
+
+                    }
+                }
+            }
+
+        }
+
+    }
+
+}
