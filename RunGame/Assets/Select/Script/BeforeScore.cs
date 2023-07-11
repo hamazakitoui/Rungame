78e8a2cfef8b54d2058d3d3d2a18ce2dbc65ee3e
hamazakitoui <nkc20206045@denpa.jp>
2023-07-11 15:03:05 +0900

    スコアの表示

--- RunGame/Assets/Select/Script/BeforeScore.cs
+++ RunGame/Assets/Select/Script/BeforeScore.cs
@@ -5,13 +5,12 @@ using UnityEngine.UI;
 
 public class BeforeScore : MonoBehaviour
 {
-    SaveData Data;
-    [SerializeField] Sprite NotAchievement;
-    [SerializeField] Sprite GetAchievement;
-    [SerializeField] GameObject[] Achevementobject;
-    [SerializeField] Text[] textobject;
+    SaveData Data;//セーブデータ
+    [SerializeField] Sprite GetAchievement;//収集物入手
+    [SerializeField] GameObject[] Achevementobject;//各ステージの収集物
+    [SerializeField] Text[] textobject;//スコア
     int nowCliar;
-    const int MaxStage = 5;
+    const int MaxStage = 4;
     const int MaxAchievement = 3;
     // Start is called before the first frame update
     void Start()
@@ -19,21 +18,26 @@ public class BeforeScore : MonoBehaviour
         SaveManager.Instance.Load();
         Data = SaveManager.Instance.GetData;
 
+        //クリアしたステージ数チェック
         for (int Stage = 0; MaxStage > Stage; Stage++)
         {
-            Achevementobject[Stage].SetActive(true);
-            textobject[Stage].gameObject.SetActive(true);
             textobject[Stage].text = Data.GetStageScore(Stage).ToString();
+
+
             if (Data.GetStageClear(Stage))
             {
-                
                 nowCliar++;
-                
 
+
+                for (int nowcliarstage = 0; nowcliarstage <= nowCliar; nowcliarstage++)
+                {
+                    Achevementobject[nowcliarstage].SetActive(true);
+                }
+
+                //収集物チェック
                 for (int Ahevement = 0; Ahevement <= MaxAchievement; Ahevement++)
                 {
                     bool checkAchevement = Data.GetAchievement(Stage, Ahevement);
-                    Debug.Log(Data.GetAchievement(Stage,Ahevement));
                     if (checkAchevement)
                     {
                         Debug.Log("abc");
